94178dcc19a96bf2aa2ec44a08d063c78e7b186f
Joshua Segu <segujoshua30@gmail.com>
2022-08-05 20:51:49 +0000

    I added adduser.php to it edited it

--- admin/adduser.php
+++ admin/adduser.php
@@ -30,33 +30,22 @@
                             <div class="control-group">
                                 <label class="control-label">Last Name :</label>
                                 <div class="controls">
-                                    <input type="text" class="span11" placeholder="Last name" />
+                                    <input type="text" class="span11" placeholder="Last name" name="lastname" />
                                 </div>
+
+                                <div class="control-group">
+                                    <label class="control-label">User Name :</label>
+                                    <div class="controls">
+                                        <input type="text" class="span11" placeholder="User name" name="username" />
+                                    </div>
                             </div>
                             <div class="control-group">
                                 <label class="control-label">Password input</label>
                                 <div class="controls">
-                                    <input type="password"  class="span11" placeholder="Enter Password"  />
+                                    <input type="password"  class="span11" placeholder="Enter Password"  name="password" />
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label">Company info :</label>
-                                <div class="controls">
-                                    <input type="text" class="span11" placeholder="Company name" />
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <label class="control-label">Description field:</label>
-                                <div class="controls">
-                                    <input type="text" class="span11" />
-                                    <span class="help-block">Description field</span> </div>
-                            </div>
-                            <div class="control-group">
-                                <label class="control-label">Message</label>
-                                <div class="controls">
-                                    <textarea class="span11" ></textarea>
-                                </div>
-                            </div>
+
                             <div class="form-actions">
                                 <button type="submit" class="btn btn-success">Save</button>
                             </div>
